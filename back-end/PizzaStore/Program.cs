8f790947c604a347573c71fdfd714af9a9df0220
ricardo <ricardofs547@gmail.com>
2024-04-26 05:01:14 -0300

    initial commit

---
+++ back-end/PizzaStore/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
+
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: MyAllowSpecificOrigins,
+    policy  =>
+    {
+      policy.WithOrigins("http://localhost:4200");
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+  c.SwaggerDoc("v1", new OpenApiInfo {
+    Title = "PizzaStore API",
+    Description = "Making the Pizzasyou love",
+    Version = "v1"
+  });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) {
+  app.UseSwagger();
+  app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+  });
+}
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
+app.Run();
